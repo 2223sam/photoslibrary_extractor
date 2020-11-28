f90db57101554d63e502487b4af28f787636fa8a
elias <swiss.elk@hotmail.com>
2020-11-28 23:46:07 +0100

    Update photoslibrary_extractor.sh

--- photoslibrary_extractor.sh
+++ photoslibrary_extractor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-resize -s 90 150
+cd ~/Downloads
 clear
-echo "photoslibrary_extractor by elias"
+echo -e "\e[7m---photoslibrary_extractor by elias---"
 sleep 2s
 echo
 echo
@@ -16,54 +16,51 @@ read
 clear
 echo "please give me your password for root access"
 sudo clear
-cd ~/Downloads/
-mkdir dinifotos
+mkdir ~/Downloads/YourLibrary
 clear
 echo "!!! BACKUP THE LIBRARY ON A USB OR SIMILAR!!!"
 sleep 3s
 echo
 echo
 echo
+read
 echo "press ENTER to continue"
-read
 clear
-echo "as soon as you press enter a new window opens. please drag and drop your library into it and come back to terminal."
+echo "as soon as you press ENTER a new window opens. please drag and drop your library into it and come back to terminal."
 read
-open ~/Downloads/dinifotos
+open ~/Downloads/YourLibrary
 clear
 echo "the library is in it? please press ENTER to contiue"
 read
 clear
+mv ~/Downloads/YourLibrary/*.photoslibrary ~/Downloads/YourLibrary/123.photoslibrary
+rm -r ~/Downloads/YourLibrary/123.photoslibrary/resources/derivatives/masters/
+cd ~/Downloads/YourLibrary/
+mkdir ~/Downloads/YourMedia/
+mkdir ~/Downloads/YourMedia/Photos/
+mkdir ~/Downloads/YourMedia/Videos/
+clear
 echo "now the process can begin. please press ENTER"
 read
-clear
-mv ~/Downloads/dinifotos/*.photoslibrary ~/Downloads/dinifotos/123.photoslibrary
-cd ~/Downloads/dinifotos/123.photoslibrary/resources/derivatives/
-rm -r masters/
-cd ~/Downloads/dinifotos/123.photoslibrary
-clear
-cd ~/Downloads/dinifotos/
-clear
-sudo find . -name '*.*' -exec cp {} ~/Downloads/dinifotos/ \;
-mkdir ~/Downloads/Photos/
-cp ~/Downloads/dinifotos/*.jpg ~/Downloads/Photos/
-cp ~/Downloads/dinifotos/*.jpeg ~/Downloads/Photos/
-cp ~/Downloads/dinifotos/*.png ~/Downloads/Photos/
-cp ~/Downloads/dinifotos/*.heic ~/Downloads/Photos/
-cp ~/Downloads/dinifotos/*.gif ~/Downloads/Photos/
-cp ~/Downloads/dinifotos/*.tiff ~/Downloads/Photos/
-cp ~/Downloads/dinifotos/*.raw ~/Downloads/Photos/
-cp ~/Downloads/dinifotos/*.mp4 ~/Downloads/Photos/
-cp ~/Downloads/dinifotos/*.mov ~/Downloads/Photos/
-cp ~/Downloads/dinifotos/*.avi ~/Downloads/Photos/
-cp ~/Downloads/dinifotos/*.wmv ~/Downloads/Photos/
-cp ~/Downloads/dinifotos/*.mpg ~/Downloads/Photos/
-cp ~/Downloads/dinifotos/*.swf ~/Downloads/Photos/
-cd ~/Downloads/
-sudo rm -r dinifotos
+sudo find . -name '*.jpg' -exec cp {} ~/Downloads/YourMedia/Photos/ \;
+sudo find . -name '*.jpeg' -exec cp {} ~/Downloads/YourMedia/Photos/ \;
+sudo find . -name '*.png' -exec cp {} ~/Downloads/YourMedia/Photos/ \;
+sudo find . -name '*.heic' -exec cp {} ~/Downloads/YourMedia/Photos/ \;
+sudo find . -name '*.gif' -exec cp {} ~/Downloads/YourMedia/Photos/ \;
+sudo find . -name '*.tiff' -exec cp {} ~/Downloads/YourMedia/Photos/ \;
+sudo find . -name '*.raw' -exec cp {} ~/Downloads/YourMedia/Photos/ \;
+sudo find . -name '*.mp4' -exec cp {} ~/Downloads/YourMedia/Videos/ \;
+sudo find . -name '*.mov' -exec cp {} ~/Downloads/YourMedia/Videos/ \;
+sudo find . -name '*.avi' -exec cp {} ~/Downloads/YourMedia/Videos/ \;
+sudo find . -name '*.wmv' -exec cp {} ~/Downloads/YourMedia/Videos/ \;
+sudo find . -name '*.mpg' -exec cp {} ~/Downloads/YourMedia/Videos/ \;
+sudo find . -name '*.swf' -exec cp {} ~/Downloads/YourMedia/Videos/ \;
+echo press ENTER to delete the unusable files
+read
+sudo rm -r ~/Downloads/YourLibrary
 clear
 echo "FINISH"
 sleep 1
 echo "press ENTER to open the Folder with the files. stored in Downloads -> Photos"
 read
-open ~/Downloads/Photos
+open ~/Downloads/YourMedia/
